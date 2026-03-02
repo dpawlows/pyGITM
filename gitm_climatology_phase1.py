eccd3d531b225910bea2aa3492be7645fba389d7
Dave Pawlowski <dpawlows@emich.edu>
2026-03-02 13:15:40 -0500

    comma

--- gitm_climatology_phase1.py
+++ gitm_climatology_phase1.py
@@ -282,7 +282,7 @@ def main():
             Ls=("time", mode_data[reference_mode]["Ls"]),
             year=("time", mode_data[reference_mode]["year"]),
             sol=("time", mode_data[reference_mode]["sol"]),
-            nfiles=("time",mode_data[reference_mode]["nfiles"])
+            nfiles=("time",mode_data[reference_mode]["nfiles"]),
             latitude=("latitude", latitude),
             altitude=("altitude", altitude),
             mode_lat=("mode_lat", lat_modes),
